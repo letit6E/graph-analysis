464e77321f0b7e36523e514f04cd93316a3b85a4
Rustam Shangareev <rustam.letitbe@gmail.com>
2024-05-01 23:02:21 +0300

    Implement sandia algorithm

--- source/main.cpp
+++ source/main.cpp
@@ -18,8 +18,22 @@ auto triangle_count_burkhardt(int n, int m, bool is_directed) -> int {
     return A.sum() / 6;
 }
 
+auto triangle_count_sandia(int n, int m) -> int {
+    Eigen::MatrixXd A(n + 1, n + 1);
+
+    for (int _ = 0; _ < m; ++_) {
+        int u, v;
+        std::cin >> u >> v;
+        if (u > v) std::swap(u, v);
+        A(v, u) += 1;
+    }
+
+    A = (A * A).array() * A.array();
+    return A.sum();
+}
+
 auto main(int argc, char** argv) -> int {
     int n, m;
     std::cin >> n >> m;
-    std::cout << triangle_count_burkhardt(n, m, false);
+    std::cout << triangle_count_sandia(n, m);
 }
