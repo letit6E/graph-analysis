1bff9b95f41e864467b53aa755ac00aee525e5c8
Rustam Shangareev <rustam.letitbe@gmail.com>
2024-05-01 22:56:02 +0300

    Implement burkhardt algorithm

--- source/main.cpp
+++ source/main.cpp
@@ -1,11 +1,25 @@
 #include <iostream>
 #include <string>
+#include <eigen-3.4.0/Eigen/Dense>
 
 #include "lib.hpp"
 
-auto main() -> int {
-    auto const lib = library{};
-    auto const message = "Hello from " + lib.name + "!";
-    std::cout << message << '\n';
-    return 0;
+auto triangle_count_burkhardt(int n, int m, bool is_directed) -> int {
+    Eigen::MatrixXd A(n + 1, n + 1);
+
+    for (int _ = 0; _ < m; ++_) {
+        int u, v;
+        std::cin >> u >> v;
+        A(u, v) += 1;
+        A(v, u) += static_cast<int>(!is_directed);
+    }
+
+    A = (A * A).array() * A.array();
+    return A.sum() / 6;
+}
+
+auto main(int argc, char** argv) -> int {
+    int n, m;
+    std::cin >> n >> m;
+    std::cout << triangle_count_burkhardt(n, m, false);
 }
